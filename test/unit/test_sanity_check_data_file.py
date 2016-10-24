86a3a294477597dfca9cc705d0c44251d96dcbb0
candicegjing <jingge2@illinois.edu>
2016-10-24 10:50:50 -0500

    Update test case

--- test/unit/test_sanity_check_data_file.py
+++ test/unit/test_sanity_check_data_file.py
@@ -4,39 +4,31 @@ import pandas as pd
 import numpy.testing as npytest
 import data_cleanup_toolbox as data_cln
 
+
 class TestSanity_check_data_file(unittest.TestCase):
     def setUp(self):
-        self.input_df_good = pd.DataFrame([["ENSG00000000003", 1, 0],
-                                           ["ENSG00000000457", 0, 0],
-                                           ["ENSG00000000005", 1, 1]])
+        self.input_df_good = pd.DataFrame([[1, 0],
+                                           [0, 0],
+                                           [1, 1]],
+                                          index=['ENSG00000000003', "ENSG00000000457", 'ENSG00000000005'],
+                                          columns=['a', 'b']
+                                          )
 
         self.run_parameters = {
+            "spreadsheet_name_full_path": "../data/spreadsheets/example.tsv",
             "results_directory": "./",
             "redis_credential": {
                 "host": "knownbs.dyndns.org",
                 "port": 6380,
                 "password": "KnowEnG"
             },
+            "source_hint": "",
+            "taxonid": '9606'
         }
-        self.output_mapped = "./tmp_mapped"
-        self.output_unmapped_one = "./tmp_unmapped_one"
-        self.output_unmapped_many = "./tmp_unmapped_many"
-
-        self.golden_output_good = pd.DataFrame([["ENSG00000000003", 1, 0, "ENSG00000000003"],
-                                      ["ENSG00000000457", 0, 0, "ENSG00000000457"],
-                                      ["ENSG00000000005", 1, 1, "ENSG00000000005"]])
 
     def tearDown(self):
         del self.input_df_good
         del self.run_parameters
-        del self.golden_output_good
-        os.remove(self.output_mapped)
-        os.remove(self.output_unmapped_one)
-        os.remove(self.output_unmapped_many)
-        del self.output_mapped
-        del self.output_unmapped_one
-        del self.output_unmapped_many
-
 
     def test_sanity_check_data_file(self):
         ret_val, ret_msg = data_cln.sanity_check_data_file(self.input_df_good, self.run_parameters)
