b0f9f71e52ddfd0cc319c70c12e4e69dbae9ea86
candicegjing <candicegjing@gmail.com>
2017-10-04 13:56:41 -0700

    Adding signature_analysis_pipeline to data cleanup pipeline

--- test/integration/verify_benchmarks.py
+++ test/integration/verify_benchmarks.py
@@ -19,6 +19,7 @@ DC_options_dict = {
     'run_gene_prioritization_pipeline_t_test': 'gene_prioritization_pipeline_t_test',
     'run_geneset_characterization_pipeline': 'geneset_characterization_pipeline',
     'run_phenotype_prediction_pipeline': 'phenotype_prediction_pipeline',
+    'run_signature_analysis_pipeline': 'signature_analysis_pipeline',
     'run_file_format_test': 'file_format_test'}
 
 verify_root_dir = '../data/verification'
