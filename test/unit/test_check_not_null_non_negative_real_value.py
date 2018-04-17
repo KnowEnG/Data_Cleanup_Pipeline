49a2c3917397f1fa308e03348f26505b5ba6b6eb
candicegjing <candicegjing@gmail.com>
2018-04-17 12:32:23 -0700

    Update unit tests based on new code changes

--- test/unit/test_check_not_null_non_negative_real_value.py
+++ test/unit/test_check_not_null_non_negative_real_value.py
@@ -3,7 +3,7 @@ import pandas as pd
 import data_cleanup_toolbox as data_cln
 
 
-class Testcheck_check_input_data_value(unittest.TestCase):
+class Testcheck_check_user_spreadsheet_data(unittest.TestCase):
     def setUp(self):
         self.input_df = pd.DataFrame([[1, 2],
                                       [0, 10],
@@ -31,28 +31,28 @@ class Testcheck_check_input_data_value(unittest.TestCase):
         del self.input_df_nan
 
     def test_check_not_null_non_negative_real_number(self):
-        ret_df = data_cln.check_input_data_value(self.input_df, check_na=True, dropna_colwise=False,
+        ret_df = data_cln.check_user_spreadsheet_data(self.input_df, check_na=True, dropna_colwise=False,
                                                  check_real_number=True,
                                                  check_positive_number=True)
         ret_flag = ret_df is not None
         self.assertEqual(True, ret_flag)
 
     def test_check_nan_input_value_in_spreadsheet(self):
-        ret_df = data_cln.check_input_data_value(self.input_df_nan, check_na=True, dropna_colwise=False,
+        ret_df = data_cln.check_user_spreadsheet_data(self.input_df_nan, check_na=True, dropna_colwise=False,
                                                  check_real_number=False,
                                                  check_positive_number=False)
         ret_flag = ret_df is not None
         self.assertEqual(False, ret_flag)
 
     def test_check_text_input_value_in_spreadsheet(self):
-        ret_df = data_cln.check_input_data_value(self.input_df_text, check_na=False, dropna_colwise=False,
+        ret_df = data_cln.check_user_spreadsheet_data(self.input_df_text, check_na=False, dropna_colwise=False,
                                                  check_real_number=True,
                                                  check_positive_number=False)
         ret_flag = ret_df is not None
         self.assertEqual(False, ret_flag)
 
     def test_check_negative_input_value(self):
-        ret_df = data_cln.check_input_data_value(self.input_df_negative, check_na=False, dropna_colwise=False,
+        ret_df = data_cln.check_user_spreadsheet_data(self.input_df_negative, check_na=False, dropna_colwise=False,
                                                  check_real_number=False,
                                                  check_positive_number=True)
         ret_flag = ret_df is not None
